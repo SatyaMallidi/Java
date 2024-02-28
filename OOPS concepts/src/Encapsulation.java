ddb81f630600b19f8a82b6a9990330617db5f801
SatyaMallidi <156490218+SatyaMallidi@users.noreply.github.com>
2024-02-28 17:59:56 -0600

    Add files via upload

---
+++ OOPS concepts/src/Encapsulation.java
@@ -0,0 +1,32 @@
+public class Encapsulation {
+    private String name;
+    private double balance;
+
+    public Encapsulation(String name,double balance){
+        this.name = name;
+        this.balance = balance;
+    }
+     public void getName(){
+        this.name = name;
+     }
+     public void getBalance(){
+        this.balance = balance;
+     }
+     public void setName(String name){
+        this.name = name;
+        System.out.println(name);
+    }
+    public void setBalance(double balance){
+        this.balance = balance;
+        System.out.println(balance);
+    }
+    public static void main(String[] args){
+        Encapsulation encapsulation = new Encapsulation("Satish",15000);
+        encapsulation.getName();
+        encapsulation.getBalance();
+        encapsulation.setName("Satya");
+        encapsulation.setBalance(10000);
+
+
+    }
+}
